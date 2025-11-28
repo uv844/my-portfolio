0f90d81d53f871c1cbed77d04d055ba2e06913fe
uv844 <yuvrajch1503@gmail.com>
2025-11-29 01:09:00 +0530

    Ensure portfolio component is entry + fix assets + add framer-motion

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-
+// src/main.jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Portfolio from './components/portfolio'; // <-- path to your file
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
-)
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+);
